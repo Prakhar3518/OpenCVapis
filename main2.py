048b6f9f94960f215a1e8e0e66da67a7866bd443
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-03-22 00:37:58 +0530

    image manipulation

--- main2.py
+++ main2.py
@@ -4,6 +4,7 @@ myimage2 = cv2.imread('assets/wowpic.jpg',-1)
 
 imagepart = myimage2[100:300,150:400] # new image bnai aur usme purani image ka beech ka part save krlia
 myimage2[150:350,250:500] = imagepart # uss part ko orignal image ke andr paste krdia
+# jo copy kra aur jo paste krre h uska dimmension same hona chahiye..mtlb 100 - 300 = 150 - 300 same pixels
 
 cv2.imshow('imagemodified',myimage2)
 cv2.waitKey(0)
